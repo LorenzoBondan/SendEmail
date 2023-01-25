eabe806c431d7a2e607a8db9dacd03e7e6868ba9
LorenzoBondan <lbbondan@gmail.com>
2023-01-25 17:30:30 -0300

    Emails sending, DTO, Service, Resource

---
+++ src/main/java/com/devsuperior/integrations/Integrations1Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.integrations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Integrations1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Integrations1Application.class, args);
+	}
+
+}
